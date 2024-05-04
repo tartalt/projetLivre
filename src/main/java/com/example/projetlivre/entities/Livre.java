e1f5b54003b8592ad6b5134b3d150e6f7c0b1b8e
tartalt <127695007+tartalt@users.noreply.github.com>
2024-05-05 00:56:58 +0200

    manque echange et front

--- src/main/java/com/example/projetlivre/entities/Livre.java
+++ src/main/java/com/example/projetlivre/entities/Livre.java
@@ -1,6 +1,7 @@
 package com.example.projetlivre.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +16,13 @@ public class Livre {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank(message = "error titre")
     private String titre;
+    @NotBlank(message = "error titre")
     private String auteur;
+    @NotBlank(message = "error titre")
     private String description;
-    private boolean demande;
+    private boolean disponible;
     @ManyToOne
     private Owner owner;
 }
