2e981b06a5d94e53ddd765f2fded878ff63b825b
tartalt <127695007+tartalt@users.noreply.github.com>
2024-05-04 13:28:57 +0200

    new test

--- src/main/java/com/example/projetlivre/security/services/UserDetailserviceImpl.java
+++ src/main/java/com/example/projetlivre/security/services/UserDetailserviceImpl.java
@@ -3,23 +3,13 @@ package com.example.projetlivre.security.services;
 import com.example.projetlivre.security.entites.Role;
 import com.example.projetlivre.security.entites.User;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
-@AllArgsConstructor
 @Service
+@AllArgsConstructor
 public class UserDetailserviceImpl implements UserDetailsService {
     private AccountService accountService;
     @Override
@@ -28,16 +18,10 @@ public class UserDetailserviceImpl implements UserDetailsService {
         User user= accountService.loadUserByUsername(username);
         if (user==null)throw new UsernameNotFoundException("user not found");
 
-        List<Role> roles = user.getRoles();
-        List<GrantedAuthority> authorities = roles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getRole()))
-                .collect(Collectors.toList());
+        return org.springframework.security.core.userdetails.User
+                .withUsername(username).password(user.getPassword())
+                .roles(user.getRoles().stream().map(Role::getRole).toArray(String[]::new))
+                .build();
 
-        boolean can = user.isCan(); // Assuming the getter method is 'isCan'
-
-        String id = user.getId(); // Assuming the getter method is 'getId'
-
-        return new CustomUserDetails( user.getUsername(),
-                user.getPassword(), authorities,can,id);
     }
 }
